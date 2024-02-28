bd4860ce59cc951c1b09c20b3706508d16d5dcca
MYKHAILOV <kmykhailov@elektroline.cz>
2024-02-28 19:30:35 +0100

    Fix `normalize_text` - handle "None" - perform casefold after decoding

--- tests/test_product_page_html_parser.py
+++ tests/test_product_page_html_parser.py
@@ -3,7 +3,29 @@ import os
 import pytest
 
 from freshpointsync.product import Product
-from freshpointsync.parser import ProductPageHTMLParser
+from freshpointsync.parser import ProductPageHTMLParser, normalize_text
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("", ""),
+        (None, ""),
+        ("foo", "foo"),
+        ("Foo", "foo"),
+        ("FoO", "foo"),
+        ("FOO", "foo"),
+        ("   baR   ", "bar"),
+        (12345, "12345"),
+        (1.23, "1.23"),
+        ("mě", "me"),
+        ("Hovězí ", "hovezi"),
+        ("  v   zakysané   smetaně  ", "v   zakysane   smetane"),
+        ("Bramborová placka se salámem", "bramborova placka se salamem")
+    ]
+)
+def test_normalize_text(text, expected):
+    assert normalize_text(text) == expected
 
 
 @pytest.fixture(scope='module')

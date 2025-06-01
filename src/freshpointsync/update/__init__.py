b36d9664af11226f159596a444d0ac6ca6ea1aad
Konstantin Mykhailov <constantinemykhailov@gmail.com>
2025-06-01 16:18:56 +0200

    Updated product update event system package.

--- src/freshpointsync/update/__init__.py
+++ src/freshpointsync/update/__init__.py
@@ -3,27 +3,17 @@ changes in FreshPoint product data. It is a part of the low-level API.
 """
 
 from ._update import (
-    AsyncHandler,
-    Handler,
-    HandlerValidator,
-    ProductCacheUpdater,
-    ProductUpdateContext,
-    ProductUpdateEvent,
-    ProductUpdateEventPublisher,
-    SyncHandler,
+    ItemUpdateContext,
+    ItemUpdatePublisher,
+    is_valid_filter,
     is_valid_handler,
     logger,
 )
 
 __all__ = [
-    'AsyncHandler',
-    'Handler',
-    'HandlerValidator',
-    'ProductCacheUpdater',
-    'ProductUpdateContext',
-    'ProductUpdateEvent',
-    'ProductUpdateEventPublisher',
-    'SyncHandler',
+    'ItemUpdateContext',
+    'ItemUpdatePublisher',
+    'is_valid_filter',
     'is_valid_handler',
     'logger',
 ]

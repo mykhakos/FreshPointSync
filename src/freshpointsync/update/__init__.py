7a16d373d039f73f3e8a3a9a299e81327d869583
Konstantin Mykhailov <constantinemykhailov@gmail.com>
2025-10-28 11:31:49 +0100

    Rename annotations module to types

--- src/freshpointsync/update/__init__.py
+++ src/freshpointsync/update/__init__.py
@@ -1,14 +1,11 @@
 """Update management module for FreshPointSync."""
 
-from .._callable_runner import run_safe, run_unsafe
-from . import annotations
+from . import types
 from ._update import is_valid_filter, is_valid_handler, logger
 
 __all__ = [
-    'annotations',
     'is_valid_filter',
     'is_valid_handler',
     'logger',
-    'run_safe',
-    'run_unsafe',
+    'types',
 ]
